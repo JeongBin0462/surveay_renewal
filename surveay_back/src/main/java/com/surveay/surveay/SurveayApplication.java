e38a412de3c3f60ee35a7b309e8b7aa5d4bc510a
JungBin0462 <dlwjdqls783@gmail.com>
2023-12-08 20:05:17 +0900

    첫번째 커밋

---
+++ surveay_back/src/main/java/com/surveay/surveay/SurveayApplication.java
@@ -0,0 +1,13 @@
+package com.surveay.surveay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveayApplication.class, args);
+	}
+
+}
---
+++ surveay_back/src/test/java/com/surveay/surveay/SurveayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surveay.surveay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
